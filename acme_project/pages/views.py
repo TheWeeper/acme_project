a0b111a3663dd17e38e5648dddb7e170f127516f
Viktor Nikolaev <vitanikolaev386@gmail.com>
2024-11-19 17:25:27 +0300

    many changes

--- acme_project/pages/views.py
+++ acme_project/pages/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from typing import Any
+from django.views.generic import TemplateView
 
 
-def homepage(request):
-    return render(request, 'pages/index.html')
+from birthday.models import Birthday
+
+
+class HomePage(TemplateView):
+    template_name = 'pages/index.html'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['total_count'] = Birthday.objects.count()
+        return context
